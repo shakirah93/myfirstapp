c75b33ebc670eeb0cbeaafe25b6833929ddce533
oyinkan <skusman@student.lautech.edu.ng>
2024-06-25 21:28:36 +0100

    added the navigation bar

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,23 @@
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <nav>
+      <img src="" alt="My website logo" />
+
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
